7f014cd31f72856312936eb3f112aeed939eef2d
Jhoseff Thiago <jhoseffthiago@gmail.com>
2025-02-22 17:19:34 -0300

    Aprendendo a sintaxe e forma de declarar Array

--- Secao03/index.js
+++ Secao03/index.js
@@ -1,3 +1,9 @@
 "use strict";
 //1. Arrays
-console.log("Oi Jhoseff a");
+let numbers = [1, 2, 3, 4];
+numbers.push(5);
+console.log(numbers[4]);
+//2. OUtra sintaxe de array
+const nums = [100, 200];
+nums.push(300);
+console.log(nums);
--- Secao03/index.ts
+++ Secao03/index.ts
@@ -3,4 +3,13 @@ let numbers: number[] = [1, 2, 3, 4]
 
 numbers.push(5)
 
-console.log(numbers[2])
+console.log(numbers[4])
+
+//2. OUtra sintaxe de array
+
+const nums: Array<number> = [100, 200]
+
+nums.push(300)
+
+console.log(nums)
+
